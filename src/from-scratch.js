2ed9aa4a5c7d25e4415177855ae39a088623e8ea
Jahlapeno1 <jimenezaot@gmail.com>
2024-06-17 20:43:05 -0400

    FINISHED

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,19 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options ={}) => {
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`)
+    }
+    // get the headers from the response.headers
+    const isJson = (response.headers.get('content-type') || '').includes('application/json');
+    if (isJson) {
+      const jsonData = await response.json()
+      return [jsonData, null]
+    }
+    const textData = await response.text()
+    return [textData, null]
+  } catch (error) {
+    console.warn(error)
+    return [null, error]
+  }
 };
